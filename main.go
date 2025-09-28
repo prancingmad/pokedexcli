ce49024c32d6fe5ebf4cc38dd99065eb9a59bb7a
prancingmad <prancingmad@gmail.com>
2025-09-27 23:06:14 -0500

    Chapter 2: Lesson 5

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -45,12 +46,23 @@ func init() {
 			description: "Displays all available pokemon in the location given",
 			callback:    commandExplore,
 		},
+		"catch": {
+			name:        "catch",
+			description: "Try to catch a Pokemon by name",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect",
+			description: "Shows detailed information about a caught Pokemon",
+			callback:    commandInspect,
+		},
 	}
 }
 
 type config struct {
 	Next     string
 	Previous string
+	Caught   map[string]Pokemon
 }
 
 type mapResponse struct {
@@ -60,6 +72,15 @@ type mapResponse struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
+}
+
+type Pokemon struct {
+	Name           string
+	BaseExperience int
+	Height         int
+	Weight         int
+	Stats          map[string]int
+	Types          []string
 }
 
 func cleanInput(text string) []string {
@@ -209,6 +230,125 @@ func commandExplore(cfg *config, args []string) error {
 	fmt.Printf("Pokemon in %s:\n", location)
 	for _, encounter := range parsed.PokemonEncounters {
 		fmt.Println("- " + encounter.Pokemon.Name)
+	}
+
+	return nil
+}
+
+func commandCatch(cfg *config, args []string) error {
+	if len(args) < 1 {
+		fmt.Println("Usage: catch <pokemon-name>")
+		return nil
+	}
+
+	name := strings.Join(args, "-")
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s/", name)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch pokemon: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("bad response: %s - %s", resp.Status, string(body))
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var data struct {
+		Name           string `json:"name"`
+		BaseExperience int    `json:"base_experience"`
+		Height         int    `json:"height"`
+		Weight         int    `json:"weight"`
+		Stats          []struct {
+			Stat struct {
+				Name string `json:"name"`
+			} `json:"stat"`
+			BaseStat int `json:"base_stat"`
+		} `json:"stats"`
+		Types []struct {
+			Type struct {
+				Name string `json:"name"`
+			} `json:"type"`
+		} `json:"types"`
+	}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return fmt.Errorf("failed to parse JSON: %w", err)
+	}
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", data.Name)
+
+	chance := 50.0 - float64(data.BaseExperience)/2.0
+	if chance < 5 {
+		chance = 5
+	}
+	if chance > 95 {
+		chance = 95
+	}
+
+	if rand.Float64()*100 < chance {
+		fmt.Printf("%s was caught!\n", data.Name)
+		if cfg.Caught == nil {
+			cfg.Caught = make(map[string]Pokemon)
+		}
+
+		statsMap := make(map[string]int)
+		for _, s := range data.Stats {
+			statsMap[s.Stat.Name] = s.BaseStat
+		}
+
+		var types []string
+		for _, t := range data.Types {
+			types = append(types, t.Type.Name)
+		}
+
+		cfg.Caught[data.Name] = Pokemon{
+			Name:           data.Name,
+			BaseExperience: data.BaseExperience,
+			Height:         data.Height,
+			Weight:         data.Weight,
+			Stats:          statsMap,
+			Types:          types,
+		}
+	} else {
+		fmt.Printf("%s escaped!\n", data.Name)
+	}
+
+	return nil
+}
+
+func commandInspect(cfg *config, args []string) error {
+	if len(args) < 1 {
+		fmt.Println("Usage: inspect <pokemon-name>")
+		return nil
+	}
+
+	name := strings.Join(args, "-")
+
+	pokemon, found := cfg.Caught[name]
+	if !found {
+		fmt.Println("You have not caught that pokemon")
+		return nil
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+
+	fmt.Println("Stats:")
+	for statName, statValue := range pokemon.Stats {
+		fmt.Printf("  -%s: %d\n", statName, statValue)
+	}
+
+	fmt.Println("Types:")
+	for _, t := range pokemon.Types {
+		fmt.Printf("  - %s\n", t)
 	}
 
 	return nil
